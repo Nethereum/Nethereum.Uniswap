33a67ce1375afc38b128f84b5796c936b1a0c103
Juan Blanco <juanfranblanco@hotmail.com>
2025-03-17 15:43:14 +0000

    Tuples as array

--- Nethereum.Uniswap/Permit2/ContractDefinition/PermitBatch.cs
+++ Nethereum.Uniswap/Permit2/ContractDefinition/PermitBatch.cs
@@ -6,7 +6,7 @@ namespace Nethereum.Uniswap.Core.Permit2.ContractDefinition
     [Struct("PermitBatch")]
     public partial class PermitBatch : PermitBatchBase {
 
-        [Parameter("tuple[]", "details", 1, "PermitDetails")]
+        [Parameter("tuple[]", "details", 1, "PermitDetails[]")]
         public override List<PermitDetails> Details { get; set; }
     }
 }
--- Nethereum.Uniswap/Permit2/ContractDefinition/PermitBatchTransferFrom.cs
+++ Nethereum.Uniswap/Permit2/ContractDefinition/PermitBatchTransferFrom.cs
@@ -6,7 +6,7 @@ namespace Nethereum.Uniswap.Core.Permit2.ContractDefinition
     [Struct("PermitBatchTransferFrom")]
     public partial class PermitBatchTransferFrom : PermitBatchTransferFromBase {
 
-        [Parameter("tuple[]", "permitted", 1, "TokenPermissions")]
+        [Parameter("tuple[]", "permitted", 1, "TokenPermissions[]")]
         public virtual List<TokenPermissions> Permitted { get; set; }
 
     }
